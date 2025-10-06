85f5fda11392ce3b72ef41ee0eb5b2c0a1c33caf
SOHEL RANA <sohelrana104322@gmail.com>
2025-10-06 22:29:32 +0600

    Fixed some issues in bresenham's algorithm

--- 4th Lab Report/Code/4.cpp
+++ 4th Lab Report/Code/4.cpp
@@ -3,16 +3,19 @@
 #include <windows.h>
 using namespace std;
 
-void init(void) {
+void init(void)
+{
     glClearColor(1, 1, 1, 1);
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(-100, 100, -100, 100);
 }
 
 // Bresenham Algorithm
-void bresenham(int x0, int y0, int x1, int y1) {
-    // Ensure left to right drawing for consistency 
-    if (x0 > x1 || y0 > y1) {
+void bresenham(int x0, int y0, int x1, int y1)
+{
+    // Ensure left to right drawing for consistency
+    if (x0 > x1 || y0 > y1)
+    {
         swap(x0, x1);
         swap(y0, y1);
     }
@@ -29,35 +32,43 @@ void bresenham(int x0, int y0, int x1, int y1) {
 
     glBegin(GL_POINTS);
 
-    if (m < 1) {
+    if (m < 1)
+    {
         int p = 2 * dy - dx;
 
-        while (x <= x1) {
+        while (x <= x1)
+        {
             glVertex2i(x, y);
 
             x += x_inc;
 
-            if (p < 0) {
+            if (p < 0)
+            {
                 p += 2 * dy;
             }
-            else {
+            else
+            {
                 y += y_inc;
                 p += 2 * dy - 2 * dx;
             }
         }
     }
-    else {
+    else
+    {
         int p = 2 * dx - dy;
 
-        while (x <= x1) {
+        while (x <= x1)
+        {
             glVertex2i(x, y);
 
             y += y_inc;
 
-            if (p < 0) {
+            if (p < 0)
+            {
                 p += 2 * dx;
             }
-            else {
+            else
+            {
                 x += x_inc;
                 p += 2 * dx - 2 * dy;
             }
@@ -68,32 +79,35 @@ void bresenham(int x0, int y0, int x1, int y1) {
 }
 
 // Display callback
-void display() {
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Draw axes
     glBegin(GL_LINES);
     glColor3f(0, 0, 0);
-    glVertex2f(100, 0);  glVertex2f(-100, 0);
-    glVertex2f(0, 100);  glVertex2f(0, -100);
+    glVertex2f(100, 0);
+    glVertex2f(-100, 0);
+    glVertex2f(0, 100);
+    glVertex2f(0, -100);
     glEnd();
 
-    // Red rectangle with DDA
     glColor3f(1, 0, 0);
     glPointSize(3);
-    bresenham(5, 5, 15, 20);
-    bresenham(15, 5, 5, 20);
+    bresenham(15, 15, 45, 60);
+    bresenham(45, 15, 15, 60);
 
     glFlush();
 }
 
 // Main function
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("DDA & DLD");
+    glutCreateWindow("Bresenham's Algorithm");
 
     init();
     glutDisplayFunc(display);
