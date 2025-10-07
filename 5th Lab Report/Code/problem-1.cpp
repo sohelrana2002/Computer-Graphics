0042c9a2bb9299b00ef749509ae31a850be2b61d
SOHEL RANA <sohelrana104322@gmail.com>
2025-10-07 22:12:50 +0600

    mid point circle drawing algorithm

---
+++ 5th Lab Report/Code/problem-1.cpp
@@ -0,0 +1,93 @@
+// mid point circle drawing algorithm
+
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+float x, y, r;
+
+void init(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-100, 100, -100, 100);
+}
+
+void put_pixel(float x, float y)
+{
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glPointSize(4.0);
+    glBegin(GL_POINTS);
+    glVertex2f(x, y);
+    glVertex2f(y, x);
+    glVertex2f(y, -x);
+    glVertex2f(x, -y);
+    glVertex2f(-x, y);
+    glVertex2f(-y, x);
+    glVertex2f(-y, -x);
+    glVertex2f(-x, -y);
+
+    glEnd();
+
+    glFlush();
+}
+void circle(float x, float y, float z)
+{
+    float p = 1 - r;
+    y = r;
+
+    while (x <= y)
+    {
+        put_pixel(x, y);
+
+        if (p < 0)
+        {
+            x = x + 1;
+            y = y;
+            p += 2 * x + 3;
+        }
+        else
+        {
+            x = x + 1;
+            y = y - 1;
+            p += 2 * (x - y) + 5;
+        }
+    }
+}
+
+void display()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0f, 0.0f, 0.0f);
+
+    glBegin(GL_LINES);
+
+    circle(x, y, r);
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Enter Redius: ";
+    cin >> r;
+
+    cout << "Enter Center Point of X axis:";
+    cin >> x;
+
+    cout << "Enter Center Point of Y axis:";
+    cin >> y;
+
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(10, 10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutCreateWindow("GLUT Shapes");
+
+    init();
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return 0;
+}
